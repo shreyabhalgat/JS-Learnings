f53e5cf458cd4749c0706b0b15a421a2b4a6c3d2
Shreya Bhalgat <bhalgatshreya2@gmail.com>
2022-01-26 18:21:40 +0530

    added hello and variable files

---
+++ 02BASIC/01variable.js
@@ -0,0 +1,10 @@
+// 4 Ways to Declare a JavaScript Variable:
+// Using var
+// Using let
+// Using const
+// Using nothing
+
+var nam="shreya";
+console.log(nam);
+
+
---
+++ 02BASIC/02variable2.js
@@ -0,0 +1,23 @@
+//building a user registeration form
+
+var UID="123abc";
+var firstname="shreya";
+var lastname="bhalgat";
+var contactNo="7057574755";
+var password="123456";
+var confirmpassword="123456";
+
+//priting info
+console.log(UID);
+
+console.log(`
+
+user id of the user is : ${UID}
+first name of the user is : ${firstname}
+last name of the user is: ${lastname}
+contact no of the user is:${contactNo}
+password of the user is:${password}
+
+
+`);
+
