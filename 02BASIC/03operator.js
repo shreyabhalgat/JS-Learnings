b47bcc51becea15605c5aff142ed3d65b7ee8470
Shreya Bhalgat <bhalgatshreya2@gmail.com>
2022-01-27 20:50:46 +0530

    added Operator

---
+++ 02BASIC/03operator.js
@@ -0,0 +1,18 @@
+// + - * %
+
+var num1=2;
+var num2=5;
+
+console.log(num1+num2);
+
+var sellingprice=199;
+var listingprice=799;
+
+var discountpercent=(listingprice-sellingprice)/listingprice *100; 
+
+console.log("Discout Percentage is :"+discountpercent);
+displaydispercentage=Math.round(discountpercent);
+
+console.log(displaydispercentage);
+
+console.log(typeof(discountpercent));
